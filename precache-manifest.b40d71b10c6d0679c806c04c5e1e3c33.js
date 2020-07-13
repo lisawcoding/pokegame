cde85692a3303e0a0fc5a1f3f3675888dee971f5
lisawcoding <tolisacoding@gmail.com>
2020-07-13 19:45:35 +0800

    Updates

---
+++ precache-manifest.b40d71b10c6d0679c806c04c5e1e3c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77a26f6e5a6087ecd1a4a9ca84ec7a4",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "0cf09cd8598f0c2ebf32",
+    "url": "/pokegame/static/css/main.96bd2d7a.chunk.css"
+  },
+  {
+    "revision": "41b73fa3a17deec6f051",
+    "url": "/pokegame/static/js/2.74329315.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokegame/static/js/2.74329315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf09cd8598f0c2ebf32",
+    "url": "/pokegame/static/js/main.84eca818.chunk.js"
+  },
+  {
+    "revision": "a75c8b32f8527831e662",
+    "url": "/pokegame/static/js/runtime-main.cccb3e90.js"
+  }
+]);
